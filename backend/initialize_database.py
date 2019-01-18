40ce5c257c135f35786d836668992d477515fb41
AIRCOMAI\r.marek <rmarek@aircom.ag>
2019-01-18 12:34:47 +0100

    Initialization of users from file, ready to begin flask backend development

--- backend/initialize_database.py
+++ backend/initialize_database.py
@@ -1,6 +1,7 @@
 import sqlite3
 import hashlib
 import datetime
+import csv
 import sqlite3_util as sq3util
 from os import path
 
@@ -8,6 +9,15 @@ DB_PATH = path.normpath("./database/breakfast.db")
 BREAKFASTS = sq3util.BREAKFASTS
 USERS = sq3util.USERS
 
+def load_users(filepath):
+    filepath = path.normpath(filepath)
+    users = []
+    columns = sq3util.USERS_COLUMNS[1::]
+    with open(filepath, 'r') as f:
+        reader = csv.reader(f)
+        users = [{column: field for column, field in zip(columns, fields)} for fields in reader]
+
+    return users
 
 if __name__ == "__main__":
     conn = sqlite3.connect(DB_PATH)
@@ -25,6 +35,10 @@ if __name__ == "__main__":
 
     sq3util.initialize_users_table(cursor, USERS)
     sq3util.initialize_breakfasts_table(cursor, BREAKFASTS)
+
+    pre_loaded_users = load_users('./initial_users.csv')
+    for user in pre_loaded_users:
+        sq3util.add_user(cursor, sq3util.create_user(**user))
 
     conn.commit()
     conn.close()
